ad5bc622c675a113acacd99e7e267e792bfc706c
Tyler Price <tyler.price1@outlook.com>
2024-12-31 14:16:05 -0500

    WIP day 9 part 2

--- day9/main.hs
+++ day9/main.hs
@@ -1,16 +1,11 @@
+{-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
+
+{-# HLINT ignore "Use lambda-case" #-}
 import Data.Char (isDigit, isSpace)
 import Data.Maybe (fromMaybe)
 import Data.Vector.Strict qualified as Vector
 import Debug.Trace (traceShow, traceShowId)
 import Text.Read (readMaybe)
-
-type StringVector = Vector.Vector String
-
-type Block = String
-
-data BlockSequence
-  = File [Block]
-  | Free Int
 
 mapFilter :: (a -> Maybe b) -> [a] -> [b]
 mapFilter fn [] = []
@@ -20,75 +15,138 @@ mapFilter fn (first : rest) = case fn first of
   where
     restMapped = mapFilter fn rest
 
-reorderBlocks :: StringVector -> [String]
-reorderBlocks files
-  | Vector.null files = []
-  | otherwise =
-      let (first', rest) = Vector.splitAt 1 files
-          first = Vector.head first'
-       in case first of
-            "." ->
-              let (beginning, last) = getLastBlock rest
-               in last : reorderBlocks beginning
-            num -> num : reorderBlocks rest
-  where
-    getLastBlock :: StringVector -> (StringVector, String)
-    getLastBlock chars
-      | Vector.null chars = error "Empty vector"
-      | all isDigit last = (beginning, last)
-      | otherwise = getLastBlock beginning
-      where
-        (beginning, last') = Vector.splitAt (Vector.length chars - 1) chars
-        last = Vector.head last'
+type StringVector = Vector.Vector String
 
-reorderFiles :: StringVector -> [String]
-reorderFiles files
-  | Vector.null files = []
-  | otherwise = []
+type Id = Int
 
-part1 :: StringVector -> Int
-part1 input = sum products
-  where
-    reordered = reorderBlocks input
-    numbers = mapFilter (\s -> readMaybe s :: Maybe Int) reordered
-    products = zipWith (*) numbers [0 ..]
+type BlockCount = Int
 
-part2 :: StringVector -> Int
-part2 input = 0
+data Block
+  = FileBlock Id
+  | FreeBlock
 
-processFile :: String -> StringVector
-processFile contents = Vector.fromList (concat (readFileLength idDigitPairs))
+instance Show Block where
+  show FreeBlock = "."
+  show (FileBlock id) = show id
+
+data BlockSequence
+  = File Id BlockCount
+  | Free BlockCount
+
+isBlockSequenceFree (Free _) = True
+isBlockSequenceFree (File _ _) = False
+
+instance Show BlockSequence where
+  show (Free len) = replicate len '.'
+  show (File id len) = concat (replicate len (show id))
+
+getBlocksInBlockSequence :: BlockSequence -> [Block]
+getBlocksInBlockSequence (Free len) = replicate len FreeBlock
+getBlocksInBlockSequence (File id len) = replicate len (FileBlock id)
+
+processInput :: String -> [BlockSequence]
+processInput contents = files
   where
     sanitized = filter isDigit contents
-    ids = map (`div` 2) [0 ..]
-    idDigitPairs = zip ids sanitized
+    files =
+      [ let len = read (ch : "") :: Int
+         in if even id
+              then File (id `div` 2) len
+              else Free len
+        | (id, ch) <- zip [0 ..] sanitized
+      ]
 
-    readFileLength :: [(Int, Char)] -> [[String]]
-    readFileLength [] = []
-    readFileLength ((id, ch) : rest) = block : readFreeSpace rest
-      where
-        len = read (ch : "") :: Int
-        idStr = show id
-        block = replicate len idStr
+checksum :: [Block] -> Int
+checksum blocks = sum products
+  where
+    numbers =
+      mapFilter
+        ( \block -> case block of
+            FreeBlock -> Nothing
+            FileBlock id -> Just id
+        )
+        blocks
+    products = zipWith (*) numbers [0 ..]
 
-    readFreeSpace :: [(Int, Char)] -> [[String]]
-    readFreeSpace [] = []
-    readFreeSpace ((_, ch) : rest) = block : readFileLength rest
+part1 :: [BlockSequence] -> Int
+part1 input = checksum reordered
+  where
+    blocks = Vector.fromList (concatMap getBlocksInBlockSequence input)
+    reordered = reorderBlocks blocks
+
+    reorderBlocks :: Vector.Vector Block -> [Block]
+    reorderBlocks files
+      | Vector.null files = []
+      | otherwise =
+          let (first, rest) = (Vector.head files, Vector.tail files)
+           in case first of
+                FreeBlock ->
+                  let (beginning, last) = getLastFileBlock rest
+                   in case last of
+                        FreeBlock -> error "Last should be file"
+                        f -> f : reorderBlocks beginning
+                f -> f : reorderBlocks rest
       where
-        len = read (ch : "") :: Int
-        block = replicate len "."
+        getLastFileBlock :: Vector.Vector Block -> (Vector.Vector Block, Block)
+        getLastFileBlock files
+          | Vector.null files = error "Empty vector"
+          | otherwise = case last of
+              FileBlock _ -> (beginning, last)
+              FreeBlock -> getLastFileBlock beginning
+          where
+            (beginning, last') = Vector.splitAt (Vector.length files - 1) files
+            last = Vector.head last'
+
+part2 :: [BlockSequence] -> Int
+part2 input = checksum (concatMap getBlocksInBlockSequence (traceShowId reordered))
+  where
+    blocks = Vector.fromList input
+    reordered = reorderFiles blocks
+
+    reorderFiles :: Vector.Vector BlockSequence -> [BlockSequence]
+    reorderFiles blocks
+      | Vector.length blocks <= 1 || Vector.all isBlockSequenceFree blocks = Vector.toList blocks
+      | otherwise =
+          let (first, rest) = (Vector.head blocks, Vector.tail blocks)
+           in case first of
+                Free freeLen ->
+                  let free = first
+                      (beginning, lastFile) = getLastFileBlock blocks
+                      beginningExcludingFirst = Vector.tail beginning
+                      File _ fileLen = lastFile
+                      diff = freeLen - fileLen
+                      remainingFree = Free diff
+                   in case signum diff of
+                        -1 -> traceShow ("Not enough", "free:", free, "beginning:", beginning, "lastFile:", lastFile, "reordered:", reorderFiles beginning, lastFile) reorderFiles beginning ++ [lastFile]
+                        0 -> traceShow ("Exact     ", "free:", free, "beginning:", beginning, "lastFile:", lastFile, "reordered:", reorderFiles beginningExcludingFirst) (lastFile : reorderFiles beginningExcludingFirst)
+                        1 -> traceShow ("Extra     ", "free:", free, "beginning:", beginning, "lastFile:", lastFile, "reordered:", reorderFiles (remainingFree `Vector.cons` beginningExcludingFirst)) (lastFile : reorderFiles (remainingFree `Vector.cons` beginningExcludingFirst))
+                firstFile -> traceShow ("File      ", "firstFile:", firstFile, "reordered:", reorderFiles rest) (firstFile : reorderFiles rest)
+
+    getLastFileBlock :: Vector.Vector BlockSequence -> (Vector.Vector BlockSequence, BlockSequence)
+    getLastFileBlock blocks
+      | Vector.null blocks = error "No file in blocks"
+      | otherwise =
+          let len = Vector.length blocks
+              (beginning, last') = Vector.splitAt (len - 1) blocks
+              last = Vector.head last'
+           in case last of
+                Free _ ->
+                  let (newBeginning, newLast) = getLastFileBlock beginning
+                      vconcat = (Vector.++)
+                   in (newBeginning `vconcat` Vector.fromList [last], newLast)
+                f -> (beginning, f)
 
 main = do
   testFile <- readFile "./test.txt"
-  let test = processFile testFile
+  let test = processInput testFile
 
   inputFile <- readFile "./input.txt"
-  let input = processFile inputFile
+  let input = processInput inputFile
 
   putStrLn "----- Part 1 -----"
-  print (part1 test) -- 1928
-  print (part1 input) -- 6359213660505
+  -- print (part1 (processInput testFile)) -- 1928
+  -- print (part1 (processInput inputFile)) -- 6359213660505
   putStrLn "----- Part 2 -----"
   print (part2 test) -- 2858
-  print (part2 input) -- ?
+  -- print (part2 input) -- ?
   return ()
