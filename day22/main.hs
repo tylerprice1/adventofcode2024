4d1b0f4d84c846e300de41c2e52de4a941dd89f7
Tyler Price <tyler.price1@outlook.com>
2025-02-07 18:28:38 -0500

    complete day 22 part 1

--- day22/main.hs
+++ day22/main.hs
@@ -1,6 +1,34 @@
-part1 input = input
+import Control.DeepSeq (deepseq)
+import Data.Bits (Bits (xor))
+import GHC.List (foldr')
+
+part1 input = sum (map ((!! 1999) . generate) input)
 
 part2 input = input
+
+mix :: Int -> Int -> Int
+mix a b = a `xor` b
+
+prune :: Int -> Int
+prune a = a `mod` 16777216
+
+generate :: Int -> [Int]
+generate start =
+  let next = evolve start
+   in next : generate next
+
+-- generate :: Int -> [Int]
+-- generate start = foldr' (\_ acc -> evolve (head acc) : acc) [start] [1 ..]
+
+evolve :: Int -> Int
+evolve a =
+  let -- Calculate the result of multiplying the secret number by 64. Then, mix this result into the secret number. Finally, prune the secret number.
+      step1 = prune ((a * 64) `mix` a)
+      -- Calculate the result of dividing the secret number by 32. Round the result down to the nearest integer. Then, mix this result into the secret number. Finally, prune the secret number.
+      step2 = step1 `deepseq` prune ((step1 `div` 32) `mix` step1)
+      -- Calculate the result of multiplying the secret number by 2048. Then, mix this result into the secret number. Finally, prune the secret number.
+      step3 = step2 `deepseq` prune ((step2 * 2048) `mix` step2)
+   in step3 `deepseq` step3
 
 main :: IO ()
 main = do
@@ -11,10 +39,11 @@ main = do
   let input = processInput inputFile
 
   putStrLn "\n----- Part 1 -----"
-  print (part1 test) -- Expected: ?
+  print (part1 test) -- Expected: 37327623
   print (part1 input) -- Expected: ?
-  putStrLn "\n----- Part 2 -----"
-  print (part2 test) -- Expected: ?
-  print (part2 input) -- Expected: ?
+  -- putStrLn "\n----- Part 2 -----"
+  -- print (part2 test) -- Expected: ?
+  -- print (part2 input) -- Expected: ?
 
-processInput contents = contents
+processInput :: String -> [Int]
+processInput contents = map (\l -> read l :: Int) (lines contents)
