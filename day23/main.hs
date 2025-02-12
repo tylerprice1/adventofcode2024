825e3ce96c6e9b20b8e2433fac5292cbcbdf70ba
Tyler Price <tyler.price1@outlook.com>
2025-02-12 18:48:28 -0500

    wip day 23 part 1

--- day23/main.hs
+++ day23/main.hs
@@ -1,47 +1,51 @@
+{-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
+
+{-# HLINT ignore "Avoid lambda" #-}
 module Main (main) where
 
+import Data.List (sort)
 import Data.Map qualified as Map
-import Debug.Trace (traceShow)
+import Debug.Trace (trace, traceShow, traceShowId)
 
-newtype Computer = Computer String
-  deriving (Eq, Ord)
+data Computer = Computer {name :: String, connections :: [Computer]}
+
+connectTo from (Computer n cs) = Computer n (sort (from : cs))
+
+instance Eq Computer where
+  (==) (Computer a _) (Computer b _) = a == b
+
+instance Ord Computer where
+  compare (Computer a _) (Computer b _) = compare a b
 
 instance Show Computer where
-  show (Computer s) = s
+  show (Computer s cs) =
+    "Computer "
+      ++ s
+      ++ " -> "
+      ++ show (map name cs)
 
-data Connection = Connection Computer Computer
-  deriving (Ord)
+findInterconnected :: Int -> Computer -> [[Computer]]
+findInterconnected 0 _ = []
+findInterconnected count start =
+  traceShow
+    (count, start)
+    concatMap
+    (map (start :) . findInterconnected (count - 1))
+    (connections start)
 
-instance Show Connection where
-  show (Connection c1 c2) = show c1 ++ "-" ++ show c2
+part1 = map (findInterconnected 3 . traceShowId)
 
-instance Eq Connection where
-  (==) (Connection (Computer a1) (Computer b1)) (Connection (Computer a2) (Computer b2)) =
-    (a1 == a2 && b1 == b2)
-      || (a1 == b2 && a2 == b1)
-
-newtype LAN = LAN (Map.Map Computer [Connection])
-  deriving (Show)
-
-addConnection :: LAN -> Connection -> LAN
-addConnection (LAN m) conn =
-  let (Connection c1 c2) = conn
-      m' = Map.alter (Just . maybe [conn] (conn :)) c2 m
-      m'' = Map.alter (Just . maybe [conn] (conn :)) c1 m'
-   in LAN m''
-
-part1 input =
-  let m =
-        foldr
-          ( \connection m ->
-              let (Connection c1 c2) = connection
-                  m' = Map.alter (Just . maybe [connection] (connection :)) c2 m
-                  m'' = Map.alter (Just . maybe [connection] (connection :)) c1 m'
-               in m''
-          )
-          (Map.empty :: Map.Map Computer [Connection])
-          input
-   in LAN m
+-- let m =
+--       foldr
+--         ( \connection m ->
+--             let (Connection c1 c2) = connection
+--                 m' = Map.alter (Just . maybe [connection] (connection :)) c2 m
+--                 m'' = Map.alter (Just . maybe [connection] (connection :)) c1 m'
+--              in m''
+--         )
+--         (Map.empty :: Map.Map Computer [Connection])
+--         input
+--  in LAN m
 
 part2 input = input
 
@@ -54,14 +58,35 @@ main = do
   let input = processInput inputFile
 
   putStrLn "\n----- Part 1 -----"
-  print (part1 test) -- Expected: ?
-  print (part1 input) -- Expected: ?
+  putStrLn "Test data:"
+  mapM_ print test
+  putStrLn ""
+  mapM_ print (part1 (take 1 test)) -- Expected: ?
+  -- mapM_ print (part1 input) -- Expected: ?
   -- putStrLn "\n----- Part 2 -----"
   -- print (part2 test) -- Expected: ?
   -- print (part2 input) -- Expected: ?
 
-processInput :: [Char] -> [Connection]
+processInput :: String -> [Computer]
 processInput contents =
   let lns = lines contents
-      connections = map (\(ch1 : ch2 : '-' : ch3 : ch4 : _) -> Connection (Computer [ch1, ch2]) (Computer [ch3, ch4])) lns
-   in connections
+      readline (ch1 : ch2 : '-' : ch3 : ch4 : "") = (Computer [ch1, ch2] [], Computer [ch3, ch4] [])
+      connectionless = map readline (lines contents)
+
+      connectionlessComputersMap =
+        foldr
+          (\(c1, c2) m -> Map.insert (name c2) c2 (Map.insert (name c1) c1 m))
+          Map.empty
+          connectionless
+
+      computersMap =
+        foldr
+          ( \(c1, c2) m ->
+              let m' = Map.adjust (c1 `connectTo`) (name c2) m
+                  m'' = Map.adjust (c2 `connectTo`) (name c1) m'
+               in m''
+          )
+          connectionlessComputersMap
+          connectionless
+      computers = Map.elems computersMap
+   in computers
