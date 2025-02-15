5cd452656dc4a7f1d2053c65718b6466337c2268
Tyler Price <tyler.price1@outlook.com>
2025-02-15 10:55:52 -0500

    wip day 23 part 2

--- day23/main.hs
+++ day23/main.hs
@@ -18,46 +18,33 @@ newtype LAN = LAN {connections :: Map.Map Computer (Set.Set Computer)}
 
 type Cache = Map.Map (Int, Computer) [Set.Set Computer]
 
+unique :: (Ord a) => [a] -> [a]
+unique = Set.toList . Set.fromList
+
 findInterconnected :: Int -> Computer -> LAN -> [Set.Set Computer]
 findInterconnected 0 _ _ = []
 findInterconnected 1 start _ = [Set.singleton start]
 findInterconnected count start (LAN lan) =
-  let connections = (Map.!) lan start
-      count_1 = count - 1
-   in concatMap
-        ( \c ->
-            map
-              (start `Set.insert`)
-              $ filter
-                ( \cs ->
-                    Set.size cs == count_1
-                      && start `Set.notMember` cs
-                      && all (`Set.member` connections) cs
-                )
-              $ findInterconnected count_1 c (LAN lan)
-        )
-        connections
+  let count_1 = count - 1
+      connections = (Map.!) lan start
+      interconnections = concatMap (\c -> findInterconnected count_1 c (LAN lan)) connections
+      valid =
+        filter
+          (\cs -> Set.size cs == count_1 && start `Set.notMember` cs && all (`Set.member` connections) cs)
+          interconnections
+   in map (start `Set.insert`) valid
+
+getAllInterconnected :: Int -> LAN -> [Set.Set Computer]
+getAllInterconnected count lan = unique $ concatMap (\computer -> findInterconnected count computer lan) (Map.keys (connections lan))
 
 part1 :: LAN -> Int
-part1 input =
-  length $
-    filter (any ((\(ch : _) -> ch == 't') . name)) $
-      Set.toList . Set.fromList $
-        concatMap (\k -> findInterconnected 3 k input) (Map.keys (connections input))
+part1 input = length $ filter (any ((== 't') . head . name)) $ getAllInterconnected 3 input
 
 part2 :: LAN -> (Int, [Set.Set Computer])
 part2 input =
   last $
     takeWhile (not . null . snd) $
-      map
-        ( \count ->
-            ( count,
-              traceShow count $
-                Set.toList . Set.fromList $
-                  concatMap (\k -> findInterconnected count k input) (Map.keys $ connections input)
-            )
-        )
-        [1 ..]
+      map (\count -> traceShow count (count, getAllInterconnected count input)) [1 ..]
 
 main :: IO ()
 main = do
@@ -67,10 +54,10 @@ main = do
   inputFile <- readFile "./input.txt"
   let input = processInput inputFile
 
-  -- putStrLn "\n----- Part 1 -----"
-  -- mapM_ print (Map.toList (connections test))
-  -- print (part1 test) -- Expected: ?
-  -- print (part1 input) -- Expected: ?
+  putStrLn "\n----- Part 1 -----"
+  print (part1 test) -- Expected: ?
+  print (part1 input) -- Expected: ?
+  --
   putStrLn "\n----- Part 2 -----"
   mapM_ print (part2 test) -- Expected: ?
   mapM_ print (part2 input) -- Expected: ?
